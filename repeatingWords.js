b44fb4557d897b0bed024e9e5bfbd4b07a93becf
Kiranyadav1234 <Kiran_Yadav@mckinsey.com>
2022-02-11 11:27:27 +0530

    feat :add check repetition of words in a sentence

---
+++ repeatingWords.js
@@ -0,0 +1,15 @@
+const sentence=prompt("Enter the sentence");
+function checkRepeatingWords(str){
+    let individualWords=str.split(" ");
+    let mapOfWords={};
+    for(let i=0;i<individualWords.length;i++){
+        let currentValue=mapOfWords[individualWords[i]];
+        if(currentValue)
+            return true;
+        mapOfWords[individualWords[i]]=1;
+    }
+    return false;
+}
+//const sentence=prompt("Enter the sentence");
+
+console.log(checkRepeatingWords(sentence));
